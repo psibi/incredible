d2b956e5bb6863d6656dc98172b242b1da81f4d3
Joachim Breitner <mail@joachim-breitner.de>
2015-08-09 14:07:08 +0200

    Try to use invertible-syntax
    this looks great at first, getting a parser and a pretty-printer in one
    go, but it fails for me (see mail on the mailing list), and I also worry
    that it might be very slow.

--- logic/Propositions.hs
+++ logic/Propositions.hs
@@ -1,15 +1,19 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Propositions where
 
-import Text.Parsec
-import Text.Parsec.Expr
-import Text.Parsec.String
-import Data.Functor.Identity
 import qualified Data.Map as M
 import qualified Data.Set as S
-import Control.Applicative hiding ((<|>))
+import Prelude hiding ((.),id)
+import Control.Category
+import Data.Char
 import Data.Maybe
-import Data.List
 import Data.Void
+
+import Control.Isomorphism.Partial
+import Control.Isomorphism.Partial.TH
+import Text.Syntax
+import qualified Text.Syntax.Parser.Naive
+import qualified Text.Syntax.Printer.Naive
 
 -- This could be made more abstract as in the unification-fd package
 data Term f v = Symb f [Term f v] | Var v
@@ -33,64 +37,74 @@ mapVar :: (v1 -> v2) -> Term f v1 -> Term f v2
 mapVar vf (Symb f ts) = Symb f $ map (mapVar vf) ts
 mapVar vf (Var v) = Var $ vf v
 
+$(defineIsomorphisms ''Term)
+
+letter, digit :: Syntax delta => delta Char
+letter  =  subset isLetter <$> token
+digit   =  subset isDigit <$> token
+
+parens :: Syntax delta => delta x -> delta x
+parens = between (text "(" <* skipSpace) (text ")" <* skipSpace)
+
+expression :: Syntax d => d Proposition
+expression = skipSpace *> exp (2::Int)
+  where
+    exp 0 =
+        (symb <$> (pure "⊥" <* (text "⊥" <|> text "False") <* skipSpace) <*> pure []) <|>
+        (symb <$> (pure "⊤" <* (text "⊤" <|> text "True")  <* skipSpace) <*> pure []) <|>
+        (var  <$> symName) <|>
+        (symb <$> symName <*> args) <|>
+        parens (exp 2)
+
+    exp 1 = chainl1 (exp 0) (textWithSyn "∧" "&") binarySymb
+    exp 2 = chainl1 (exp 1) (textWithSyn "∨" "|") binarySymb
+
+    symName = subset (not . special) <$> many1 (letter <|> digit) <* skipSpace
+    args = parens $ sepBy (exp 2) (comma <* skipSpace)
+
+    special s = s `elem` ["⊥", "⊤", "False", "True", "∧", "∨"]
+
+
+textWithSyn :: Syntax delta => String -> String -> delta String
+textWithSyn x y = (text x <|> text y) *> skipSpace *> pure x
+
+-- Helpers to deal with partial-isomorphisms. Tricky puzzle to generate, but as
+-- long as the type work out...
+binarySymb :: Iso (Term f v, (f, Term f v)) (Term f v)
+binarySymb = symb . (id *** twoElementList) . inverse associate .  (commute *** id) . associate
+
+twoElementList :: Iso (a, a) [a]
+twoElementList = cons . (id *** (cons . ((id *** nil).unit)))
+
+
 -- Pretty printer
 
--- Horribly inefficient, all that
 printTerm :: Proposition -> String
-printTerm (Var v) = v
-printTerm (Symb f []) = f
-printTerm (Symb f [arg1,arg2])
-    | f `S.member` infixSymbols
-    = printParens (printTerm arg1) ++ f ++ printParens (printTerm arg2)
-printTerm (Symb f args) = f ++ printParens (intercalate "," (map printTerm args))
-
-printParens :: String -> String
-printParens s = "("++s++")"
-
--- Parser
+printTerm p = fromMaybe e $ Text.Syntax.Printer.Naive.print expression p
+  where e = "Could not pretty-print " ++ show p
 
 parseTerm :: String -> Either String Proposition
-parseTerm = either (Left . show) Right . parse (between (return ()) eof termP) ""
-
--- lexeme
-l :: Parser a -> Parser a
-l = between (return ()) spaces
-
-termP :: Parser Proposition
-termP =  buildExpressionParser table atomP <?> "proposition"
-
-table :: OperatorTable String () Identity Proposition
-table = [ [ binary "∧" ["&"]
-          , binary "→" ["->"]
-          ]
-        , [ binary "∨" ["|"]
-          ]
-        ]
-  where
-    binary op alts = Infix ((\a b -> Symb op [a,b]) <$ l (choice (map string (op:alts)))) AssocLeft
-
-atomP :: Parser Proposition
-atomP = choice
-    [ string "⊥" >> return (Symb "⊥" [])
-    , string "⊤" >> return (Symb "⊤" [])
-    , try (string "False") >> return (Symb "⊥" [])
-    , try (string "True") >> return (Symb "⊤" [])
-    , between (char '(') (char ')') termP
-    , do
-        hd <- many1 alphaNum
-        let sym = fromMaybe hd $ M.lookup hd abbreviations
-        option (Var sym) $ between (char '(') (char ')') $ do
-            Symb sym <$> termP `sepBy1` (char ',')
-    ]
-
+parseTerm e = case Text.Syntax.Parser.Naive.parse expression e of
+    [] -> Left e1
+    [r] -> Right (compat r)
+    _  -> Left e2
+  where e1 = "Could not parse \"" ++ e ++ "\""
+        e2 = "Parsing \"" ++ e ++ "\" is ambiguous"
 
 -- Ground terms are terms with no variables, such as the propositions in the
 -- task. We encode that invariant in the type system.
 type GroundTerm = Term String Void
 
 parseGroundTerm :: String -> Either String GroundTerm
-parseGroundTerm = either (Left . show) (Right . fixVar) . parse termP ""
+parseGroundTerm s = fixVar `fmap` parseTerm s
 
 fixVar :: Term a a -> Term a Void
 fixVar (Symb f ts) = Symb f $ map fixVar ts
 fixVar (Var v) = Symb v []
+
+compat :: Term String a -> Term String a
+compat (Symb "and" ts) = Symb "∧" $ map compat ts
+compat (Symb "or" ts) = Symb "∨" $ map compat ts
+compat (Symb "imp" ts) = Symb "→" $ map compat ts
+compat (Symb f ts) = Symb f $ map compat ts
+compat (Var v) = Var v
